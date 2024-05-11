748eca49da9f212aa8693cbacffbb190a4545787
Ashish Nagvanshi <aashishsingh24032005@gmail.com>
2024-05-11 09:50:58 +0530

    learned how to use jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -138,13 +138,15 @@ const userSchema = new Schema(
 // now lets export this 
 
 
-export const User = mongoose.model("User", userSchema)
+
 
 
 
 userSchema.pre('save', async function(next){
 
   //which one you want to encrypt
+
+  if(!this.isModified("password")) return next()
 
   this.password = bcrypt.hash(this.password, 10)
 
@@ -156,11 +158,100 @@ userSchema.pre('save', async function(next){
 })
 
 
+// right now we have stored the password in form of encrypted formate inside the database 
+
+// but i want check the user write password is same as they storeed in database
+
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+
+     return await bcrypt.compare(password, this.password)
+
+  
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+
+ return jwt.sign(
+
+
+        {
+
+         _id: this._id,
+         email: this.email,
+         username: this.username,
+         fullName: this.fullName
+
+
+
+
+        },
+
+        process.env. ACCESS_TOKEN_SECRET,
+
+        {
+
+       expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+
+
+
+        }
+
+         
+
+  )
+
+
+
+  
+}
+
+userSchema.methods.generateRefreshToken = function () {
+
+
+  return jwt.sign(
+
+
+    {
+
+     _id: this._id,
+     
+
+
+
+
+    },
+
+    process.env.REFRESH_TOKEN_SECRET,
+
+    {
+
+   expiresIn: process.env.REFRESH_TOKEN_Expiry
+
+
+
+    }
+
+     
+
+)
 
 
 
 
 
+
+
+
+
+  
+}
+
+
+
+export const User = mongoose.model("User", userSchema)
 
 
 
